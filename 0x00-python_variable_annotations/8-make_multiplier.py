20e4a19bcdc9c9a511e0650c0051de80e098fe6d
Dani-1138 <danieldemeke92@gmail.com>
2023-10-05 21:26:32 +0000

    make multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
-'''Task 8's module.
-'''
-from typing import Callable
+""" Complex types - functions"""
+from typing import Callable, Iterator, Union, Optional, List, Tuple
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    '''Creates a multiplier function.
-    '''
+    """
+    takes a float multiplier as argument,
+    returns a function that multiplies a float by multiplier.
+    """
+    def f(n: float) -> float:
+        """ multiplies a float by multiplier """
+        return float(n * multiplier)
+
+    return f
